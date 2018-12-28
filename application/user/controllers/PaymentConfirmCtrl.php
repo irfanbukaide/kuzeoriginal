23f92134821afc46a30a675ef3a8fd146e8c6f5c
Muhammad Irfan <irfan@bukaide.com>
2018-12-28 17:05:21 +0700

    Update

--- application/user/controllers/PaymentConfirmCtrl.php
+++ application/user/controllers/PaymentConfirmCtrl.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Konfirmasi extends MY_Controller
+class PaymentConfirmCtrl extends MY_Controller
 {
     public function __construct()
     {
